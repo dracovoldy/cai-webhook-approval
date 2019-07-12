8624187629fa7373c2de8cf2ec368419d26f20bc
Mallik <abhishek.mallik@capgemini.com>
2019-07-12 21:15:23 +0530

    revert code index.js stupid fix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
-// const helmet = require('helmet');
-// const cors = require('cors');
+const helmet = require('helmet');
+const cors = require('cors');
 const bodyParser = require('body-parser'); 
-// const favicon = require('serve-favicon');
+const favicon = require('serve-favicon');
 
 //Require Routes
 const home = require('./routes/home');
@@ -12,13 +12,11 @@ const cai = require('./routes/cai');
 
 const app = express();
 app.use(bodyParser.json());  
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(helmet());
-// app.use(cors());
-// app.use(express.static('public'));
+app.use(helmet());
+app.use(cors());
+app.use(express.static('public'));
 app.use(morgan('tiny'));
-// app.use(favicon(__dirname + '/static/favicon.ico'));
+app.use(favicon(__dirname + '/static/favicon.ico'));
 
 //use - routes
 app.post('/errors', function(req, res) {
