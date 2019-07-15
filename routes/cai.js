6776982e18aff8902364f4548ffea87e0b2b769d
Mallik <abhishek.mallik@capgemini.com>
2019-07-15 15:55:50 +0530

    revert

--- routes/cai.js
+++ routes/cai.js
@@ -11,13 +11,13 @@ router.post('/getTasks', (req, res) => {
             language: "en"
         }
     };
-    
+
     let reply = {
         outputSpeech: "SSML",
         type: "text",
         content: "Mock reply"
     };
-    
+
     // sendToCai.replies.push(reply);
     // sendToCai.conversation.memory = {
     //     instanceId: "00078788",
@@ -25,7 +25,7 @@ router.post('/getTasks', (req, res) => {
     // }
     // res.send(sendToCai);    
     console.log(req.body);
-    
+
     request.get('https://p2001172697trial-trial.apim1.hanatrial.ondemand.com:443/p2001172697trial/Workflow_approval/TaskCollection?sap-client=400&$format=json&$filter=Status%20eq%20%27READY%27', {
         'auth': {
             'user': 'pritamsa',
@@ -36,21 +36,32 @@ router.post('/getTasks', (req, res) => {
         if (err) {
             return console.log(err);
         }
-        
+
         console.log(body);
-        
-        reply.content = body.d.results[0].TaskTitle;
-        sendToCai.replies.push(reply);
-        sendToCai.conversation.memory = {
-            "instanceId": body.d.results[0].InstanceID,
-            "task_index": 0
+        if (req.body.conversation.memory !== undefined) {
+            reply.content = body.d.results[0].TaskTitle;
+            sendToCai.replies.push(reply);
+            sendToCai.conversation.memory = {
+                "instanceId": body.d.results[0].InstanceID,
+                "task_index": 1
+            }
+
+            res.json(sendToCai);
+        } else {
+            reply.content = body.d.results[req.body.memory.task_index].TaskTitle;
+            sendToCai.replies.push(reply);
+            sendToCai.conversation.memory = {
+                "instanceId": body.d.results[req.body.memory.task_index].InstanceID,
+                "task_index": req.body.memory.task_index + 1
+            }
+
+            res.json(sendToCai);
         }
-       
-        res.json(sendToCai);  
-         
+
+
 
     });
-      
+
 
 });
 
