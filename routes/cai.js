38f7894e7784752a294f17bf51f4d2457e597c59
Mallik <abhishek.mallik@capgemini.com>
2019-07-18 12:55:57 +0530

    check post

--- routes/cai.js
+++ routes/cai.js
@@ -381,7 +381,7 @@ router.post('/approveTask', (req, res) => {
                     },
                     withCredentials: true
                 }
-                config.headers["x-csrf-token"] = response.headers["x-csrf-token"];
+                config2.headers["x-csrf-token"] = response.headers["x-csrf-token"];
 
                 axios.request(url2, config2)
                     .then((response) => {
